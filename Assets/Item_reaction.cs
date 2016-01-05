f98b20b215dff9fb17b80371ce1042bb2c942e29
Park-Hyeji <ehddn159@naver.com>
2016-01-05 16:18:14 +0900

    #5 탱크랑 아이템 충돌까지

---
+++ Assets/Item_reaction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Item_reaction : MonoBehaviour {
+
+    public GameObject UIDSDF;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   
+	}
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.name == "Impediments")
+        {
+            Debug.Log("crush");
+            GameObject.Destroy(other.gameObject);
+            fdsa();
+        }
+    }
+
+    private void fdsa() {
+        UIDSDF.SetActive(true);
+    }
+}
