24269899b7bdd3112b3d40dfbdbee60248d15cf5
bnitech <bntech88@gmail.com>
2016-01-11 19:10:16 +0900

    탱크 방향키 조작

---
+++ ProjectOP/Assets/02. Scripts/TankMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMove : MonoBehaviour {
+
+    public int speed = 3; // 기본 3
+    int rotateSpeed;
+	// Use this for initialization
+	void Start () {
+        rotateSpeed = speed * 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -rotateSpeed * Time.deltaTime, 0));
+        }
+    }
+}
