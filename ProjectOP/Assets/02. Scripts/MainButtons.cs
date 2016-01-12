59eb3f3f8012dab9a99ef601bada4625723911e9
bnitech <bntech88@gmail.com>
2016-01-12 14:47:04 +0900

    InGame, Tank 씬 통합

--- ProjectOP/Assets/02. Scripts/MainButtons.cs
+++ ProjectOP/Assets/02. Scripts/MainButtons.cs
@@ -5,11 +5,13 @@ public class MainButtons : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+ 
 }
