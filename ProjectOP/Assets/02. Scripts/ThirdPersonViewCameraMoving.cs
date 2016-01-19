172539b8ed726e91bdbebc03cab28e98a2a9a479
HANYONG LEE <glhy0718@gmail.com>
2016-01-19 12:09:58 +0900

    #4 자잘한 추가

--- ProjectOP/Assets/02. Scripts/ThirdPersonViewCameraMoving.cs
+++ ProjectOP/Assets/02. Scripts/ThirdPersonViewCameraMoving.cs
@@ -5,15 +5,17 @@ public class ThirdPersonViewCameraMoving : MonoBehaviour {
 
 	public bool isThirdPersonView;
 	public Camera mainCamera;
-	public float cameraRotateSpeed;
+	public float cameraRotateXSpeed;
+	public float cameraRotateYSpeed;
 
 	void Update () {
 		if (isThirdPersonView) {
+			mainCamera.transform.Rotate (0, 0, 0 - mainCamera.transform.rotation.z);
 			for (int i = 0; i < Input.touchCount; i++) {
 				Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch(i).position);
 				RaycastHit hit;
 				if (Physics.Raycast (ray, out hit) && hit.collider.tag == "Camera Moving Field") {
-					mainCamera.transform.Rotate (0, Input.GetTouch (i).deltaPosition.x * cameraRotateSpeed, 0);
+					mainCamera.transform.Rotate (Input.GetTouch (i).deltaPosition.y * cameraRotateYSpeed, Input.GetTouch (i).deltaPosition.x * cameraRotateXSpeed, 0);
 					Debug.Log (Input.GetTouch (i).deltaPosition.x);
 				}
 			}
