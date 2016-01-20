160ed9ddec8adb298cdbe45347f1ffc6c6539d7e
HANYONG LEE <glhy0718@gmail.com>
2016-01-20 19:21:46 +0900

    #4 해상도에맞게 ui수정

--- ProjectOP/Assets/02. Scripts/WheelMove.cs
+++ ProjectOP/Assets/02. Scripts/WheelMove.cs
@@ -4,43 +4,22 @@ using System.Collections;
 public class WheelMove : MonoBehaviour {
 
 	public bool isFirstPersonView;
-	public float draggingBuffer;
-	private bool[] moveInfo = new bool[20];
 
 	void Update(){
 		if (isFirstPersonView) {
 			for (int i = 0; i < Input.touchCount; i++) {
+				
 				Ray ray = Camera.main.ScreenPointToRay (Input.GetTouch (i).position);
 				RaycastHit hit;
-				if (Physics.Raycast (ray, out hit) && hit.collider.tag == "LeftWheel") {
-					if (Input.GetTouch (i).deltaPosition.y > draggingBuffer) {
-						moveInfo [i] = true;
-						Debug.Log ("왼쪽 앞");
-					} else if (Input.GetTouch (i).deltaPosition.y < 0 - draggingBuffer) {
-						moveInfo [i] = false;
-						Debug.Log ("왼쪽 뒤");
-					} else {
-						if (moveInfo [i]) {
-							Debug.Log ("왼쪽 앞");
-						} else {
-							Debug.Log ("왼쪽 뒤");
-						}
-					}
-				}
-				if (Physics.Raycast (ray, out hit) && hit.collider.tag == "RightWheel") {
-					if (Input.GetTouch (i).deltaPosition.y > draggingBuffer) {
-						moveInfo [i] = true;
-						Debug.Log ("오른쪽 앞");
-					} else if (Input.GetTouch (i).deltaPosition.y < 0 - draggingBuffer) {
-						moveInfo [i] = false;
-						Debug.Log ("오른쪽 뒤");
-					} else {
-						if (moveInfo [i]) {
-							Debug.Log ("오른쪽 앞");
-						} else {
-							Debug.Log ("오른쪽 뒤");
-						}
-					}
+
+				if (Physics.Raycast (ray, out hit) && hit.collider.tag == "FrontRight") {
+					Debug.Log ("오른쪽 앞");
+				} else if (Physics.Raycast (ray, out hit) && hit.collider.tag == "BackRight") {
+					Debug.Log ("오른쪽 뒤");
+				} else if (Physics.Raycast (ray, out hit) && hit.collider.tag == "FrontLeft") {
+					Debug.Log ("왼쪽 앞");
+				} else if (Physics.Raycast (ray, out hit) && hit.collider.tag == "BackLeft") {
+					Debug.Log ("왼쪽 뒤");
 				}
 			}
 		}
