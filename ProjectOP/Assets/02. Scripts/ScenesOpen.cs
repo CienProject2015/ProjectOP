4fac60207cc485cafa548d2838d5593fc98145b0
bnitech <bntech88@gmail.com>
2016-02-01 17:31:25 +0900

    Revert "modify"
    This reverts commit eb2fde5f02e3c47aae674ddb64e2dbe7336fb8f1.

---
+++ ProjectOP/Assets/02. Scripts/ScenesOpen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenesOpen : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Scenes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+    void Scenes()
+    {
+        Application.LoadLevelAdditive(3);
+   
+    }
+}
