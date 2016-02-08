1cce8e05ef3a2438a26dc1500d16fdfd792093b4
HANYONG LEE <glhy0718@gmail.com>
2016-02-08 22:46:05 +0900

    #7 홀로그램 효과추가

---
+++ Assets/02. Scripts/HologramAnim.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HologramAnim : MonoBehaviour {
+
+	public Vector2 animRate;
+	private Vector2 offset = Vector2.zero;
+
+	void Update () {
+		offset += animRate * Time.deltaTime;
+		GetComponent<Renderer> ().material.SetTextureOffset ("_MainTex", offset);
+	}
+}
