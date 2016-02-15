d0fcd79ed854e2848177d5e71847ffec9e7223bd
HANYONG LEE <glhy0718@gmail.com>
2016-02-15 19:13:26 +0900

    #6 인벤토리 시스템 수정

--- Assets/02. Scripts/InventoryManager.cs
+++ Assets/02. Scripts/InventoryManager.cs
@@ -13,23 +13,27 @@ public class InventoryManager : MonoBehaviour {
 	public Sprite sprite_None;
 
 	void Start(){
-		itemImages = new Sprite[16];
-		for (int i = 0; i < 16; i++)
+		itemImages = new Sprite[22];
+		for (int i = 0; i < 22; i++)
 			itemImages [i] = items [i].GetComponent <Image> ().sprite;
 
-		itemSelects = new Toggle[16];
-		for (int i = 0; i < 16; i++)
+		itemSelects = new Toggle[22];
+		for (int i = 0; i < 22; i++)
 			itemSelects [i] = items [i].GetComponent <Toggle> ();
 	}
 
 	public void GetItemImage(){
 		if (itemToggleGroup.AnyTogglesOn ()) {
-			for (int i = 0; i < 16; i++) {
+			for (int i = 0; i < 22; i++) {
 				if (itemSelects [i].isOn)
 					selectedItemImage.GetComponent <Image> ().sprite = itemImages [i];
 			}
 		}
 		else
 			selectedItemImage.GetComponent <Image> ().sprite = sprite_None;
+	}
+
+	public void GetItemInfo(){
+		
 	}
 }
