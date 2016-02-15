7403e7cfb8bebff3e76d143baf8a8c7c5e89bb04
HANYONG LEE <glhy0718@gmail.com>
2016-02-15 18:20:03 +0900

    #6 화면 얼럿표시 수정

--- Assets/02. Scripts/AlertScreenAnim.cs
+++ Assets/02. Scripts/AlertScreenAnim.cs
@@ -6,34 +6,29 @@ using UnityEngine.UI;
 public class AlertScreenAnim : MonoBehaviour {
 
 	private bool fadeout;
-	public bool AlertAnimStop;
 
 	void Start(){
 		fadeout = true;
-		AlertAnimStop = false;
 
 		gameObject.GetComponent <Image> ().color = new Color (1, 1, 1, 0);
 	}
 
 	public void StartAlertAnim(){
 		gameObject.GetComponent <Image> ().color = new Color (1, 1, 1, 1);
-		AlertAnimStop = false;
+		StopCoroutine ("AlertAnim");
 		StartCoroutine ("AlertAnim");
 	}
 
 	public void StopAlertAnim(){
-		AlertAnimStop = true;
+		StopCoroutine ("AlertAnim");
+		StartCoroutine ("AlertAnimEnd");
 	}
 
 	IEnumerator AlertAnim(){
 		while (true) {
-			if (AlertAnimStop) {
-				gameObject.GetComponent <Image> ().color = new Color (1, 1, 1, 0);
-				yield break;
-			}
 			if (fadeout) {
 				gameObject.GetComponent <Image> ().color -= new Color (0, 0, 0, 0.05f);
-				if (gameObject.GetComponent <Image> ().color.a < 0.1f) {
+				if (gameObject.GetComponent <Image> ().color.a < 0.3f) {
 					fadeout = false;
 				}
 			} else if (!fadeout) {
@@ -42,6 +37,13 @@ public class AlertScreenAnim : MonoBehaviour {
 					fadeout = true;
 				}
 			}
+			yield return null;
+		}
+	}
+
+	IEnumerator AlertAnimEnd(){
+		while (gameObject.GetComponent <Image> ().color.a > 0){
+			gameObject.GetComponent <Image> ().color -= new Color (0, 0, 0, 0.02f);
 			yield return null;
 		}
 	}
