db4967c3520d1ff1a493359e766a7fb4a4e8e2bf
bnitech <bntech88@gmail.com>
2016-02-16 15:37:11 +0900

    이것 저것

--- Assets/02. Scripts/NoiseControl.cs
+++ Assets/02. Scripts/NoiseControl.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NoiseControl : MonoBehaviour {
+using UnityStandardAssets.ImageEffects;
 
+public class NoiseControl : MonoBehaviour {
+	NoiseAndGrain noise;
 	// Use this for initialization
 	void Start () {
-		//Camera.main.GetComponent<NoiseAndGrain> ();
-		//GameObject.Find("MainCamera").GetComponent("NoiseAndGrain");
+		noise = GameObject.Find ("MainCamera").GetComponent<NoiseAndGrain> ();
 		//GameObject.Find
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		//noise.generalIntensity += 1;
 	
 	}
 }
