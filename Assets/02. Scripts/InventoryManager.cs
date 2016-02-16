39564dc8fb80afb2bd6f2ff5f6c5fd111d9f83a3
HANYONG LEE <glhy0718@gmail.com>
2016-02-16 14:07:53 +0900

    #6 튜토리얼 버튼 반짝임 구현

--- Assets/02. Scripts/InventoryManager.cs
+++ Assets/02. Scripts/InventoryManager.cs
@@ -12,7 +12,11 @@ public class InventoryManager : MonoBehaviour {
 	private Toggle[] itemSelects;
 	public Sprite sprite_None;
 
+	public Text itemName;
+	public Text itemInfo;
 
+	public GameObject item_Using_Question;
+	public GameObject M_Chip_Using_Question;
 
 	void Start(){
 		itemImages = new Sprite[22];
@@ -22,6 +26,9 @@ public class InventoryManager : MonoBehaviour {
 		itemSelects = new Toggle[22];
 		for (int i = 0; i < 22; i++)
 			itemSelects [i] = items [i].GetComponent <Toggle> ();
+
+		itemName.text = "아이템을 선택하세요.";
+		itemInfo.text = "";
 	}
 
 	public void GetItemInfo(){
@@ -32,6 +39,14 @@ public class InventoryManager : MonoBehaviour {
 			}
 		} else {
 			selectedItemImage.GetComponent <Image> ().sprite = sprite_None;
+		}
+
+		if (selectedItemImage.GetComponent <Image> ().sprite == sprite_None) {
+			itemName.text = "아이템을 선택하세요.";
+			itemInfo.text = "";
+		} else {
+			itemName.text = "아이템 선택됨";
+			itemInfo.text = "ㅇㅇㅇㅇ\ndsfsfn\ndf";
 		}
 	}
 }
---
+++ Assets/02. Scripts/TutorialButtonGuide.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class TutorialButtonGuide : MonoBehaviour {
+
+	private bool fadeout;
+
+	void Start(){
+		fadeout = true;
+	}
+
+	public void StartTwinkle(GameObject target){
+		target.transform.Find("ButtonGuide").GetComponent <Image> ().color = new Color (1, 1, 1, 1);
+		StartCoroutine (TwinkleAnim(target));
+	}
+
+	public void StopTwinkle(GameObject target){
+		target.transform.Find("ButtonGuide").GetComponent <Image> ().color = new Color (1, 1, 1, 0);
+	}
+
+	IEnumerator TwinkleAnim(GameObject target){
+		while (true) {
+			if (target.transform.Find ("ButtonGuide").GetComponent <Image> ().color.a == 0)
+				yield break;
+			if (fadeout) {
+				target.transform.Find("ButtonGuide").GetComponent <Image> ().color -= new Color (0, 0, 0, 0.02f);
+				if (target.transform.Find("ButtonGuide").GetComponent <Image> ().color.a < 0.3f) {
+					fadeout = false;
+				}
+			} else if (!fadeout) {
+				target.transform.Find("ButtonGuide").GetComponent <Image> ().color += new Color (0, 0, 0, 0.02f);
+				if (target.transform.Find("ButtonGuide").GetComponent <Image> ().color.a > 1) {
+					fadeout = true;
+				}
+			}
+			yield return null;
+		}
+	}
+}
