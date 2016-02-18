8d2231f61f519c683d4854773e571cefb644c326
Park-Hyeji <ehddn159@naver.com>
2016-02-18 15:24:25 +0900

    #10 오브젝트씬, 탱크1 이벤트 추가

---
+++ Assets/02. Scripts/CsFadeIn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CsFadeIn : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void StartFadeIn()
+    {
+        StartCoroutine("FadeIn");
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float i = 1f; i >= 0; i -= 0.1f)
+        {
+            Color color = new Vector4(1, 1, 1, i);
+            transform.GetComponent<Renderer>().material.color = color;
+            yield return 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/02. Scripts/ItemArray.cs
+++ Assets/02. Scripts/ItemArray.cs
@@ -7,6 +7,7 @@ public class ItemArray : MonoBehaviour {
     int random;
     public GameObject obj_panel;
     public GameObject obj_act_panel;
+    public GameObject obj_IceCube;
     public GameObject Items_MemoryChip;
     public GameObject Text_Trash_Active;
     public GameObject Text_Trash_hint;
@@ -47,8 +48,8 @@ public class ItemArray : MonoBehaviour {
             }
             else if (name[1].Equals("Trash"))
             {
-                //GameObject.Find("_EventSystem").SendMessage("ReceiveLog", "펭귄이 악취가 난다며 싫어 한다.없애자.");
-                Debug.Log("펭귄이 악취가 난다며 싫어 한다.없애자.");
+                GameObject.Find("_EventSystem").SendMessage("ReceiveLog", "펭귄이 악취가 난다며 싫어 한다.없애자.");
+                //Debug.Log("펭귄이 악취가 난다며 싫어 한다.없애자.");
                 if (item.Contains("Items_Lighter"))
                 {
                     obj_act_panel.SetActive(true);
@@ -134,9 +135,28 @@ public class ItemArray : MonoBehaviour {
             }
             else if (name[1].Equals("IceCube"))
             {
+                obj_IceCube.SendMessage("StartFadeIn");
                 Destroy(other.gameObject);
-                //GameObject.Find("_EventSystem").SendMessage("ReceiveLog", "펭귄이 호기심어린 눈빛으로 고래의 사체를 관찰한다.");
-                Debug.Log("펭귄이 호기심어린 눈빛으로 고래의 사체를 관찰한다.");
+                if (name[2].Equals("Whale"))
+                {
+                    //GameObject.Find("_EventSystem").SendMessage("ReceiveLog", "펭귄이 호기심어린 눈빛으로 고래의 사체를 관찰한다.");
+                    Debug.Log("펭귄이 호기심어린 눈빛으로 고래의 사체를 관찰한다.");
+                }
+                else if (name[2].Equals("Tank"))
+                {
+                    if (item.Contains("Items_Penguin"))
+                    {
+                        //GameObject.Find("_EventSystem").SendMessage("ReceiveLog", "펭귄에게 펭귄 마법을 써달라고 부택해야 겠다.(펭귄 인형을 건낸다.)");
+                        Debug.Log("펭귄에게 펭귄 마법을 써달라고 부택해야 겠다.(펭귄 인형을 건낸다.)");
+                        obj_IceCube.SendMessage("StartFadeIn",2);
+                        Destroy(other.gameObject);
+                    }
+                    else
+                    {
+                        //GameObject.Find("_EventSystem").SendMessage("ReceiveLog", "탐사정1이 빙하에 갇혀있다. 구해야 한다.");
+                        Debug.Log("탐사정1이 빙하에 갇혀있다. 구해야 한다.");
+                    }
+                }
             }
             else if (name[1].Equals("PenguinTotemOld"))
             {
