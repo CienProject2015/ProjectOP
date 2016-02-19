2e4c95b6562dc29593f7c8eadea744b83c27006b
bnitech <bntech88@gmail.com>
2016-02-20 00:06:07 +0900

    #6 안드로이드 게임 푸시 알림 구현

---
+++ Assets/02. Scripts/PushAlarmMessage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushAlarmMessage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LocalNotification.SendNotification(1, 10, "WarmyLand", "드루와드루와", new Color32(0xff, 0x44, 0x44, 255));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
