5178ee1bfae4c127cc379e4584f5ef8daebc6f73
Park-Hyeji <ehddn159@naver.com>
2016-02-21 02:44:27 +0900

    #10 맵 적용은 했으나 펭귄이 안 따라오고 texture 2가 없어짐

---
+++ Assets/02. Scripts/Load_Map.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Load_Map : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Application.LoadLevel("광장1");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
